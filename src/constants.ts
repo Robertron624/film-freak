1f260253069aa74d38e9d78a62e3816c272c8750
Roberto Ramirez <robert.624@hotmail.com>
2024-07-15 08:42:42 -0500

    fix: Handle 404 error in SearchResults component
    The SearchResults component now handles the 404 error by displaying a specific message when no results are found. This improves the user experience by providing clear feedback when the search query returns no matches.An error type was created for this purpose.
    
    Finally will convert a person known for department to their profession, for example acting will convert to actor, directing to director, etc.

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 
 export const baseApiUrl = 'https://api.themoviedb.org/3'
-export const baseApiUrlSearch = 'https://api.themoviedb.org/3/search/'
+export const baseApiUrlSearch = 'https://api.themoviedb.org/3/search/'    
 
 // get the API key from the .env file
 export const apiKey = import.meta.env.VITE_API_KEY as string
