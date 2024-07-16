9b8d8a27e9d5347a84b79b01eeabc2c13eb03c9a
Roberto Ramírez Aguilar <72587880+Robertron624@users.noreply.github.com>
2024-07-16 17:29:07 -0500

    fix: Footer and Header component filename errors

--- src/components/layout/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className='flex justify-center items-center bg-purple rounded-md px-4 py-2 mx-auto gap-2 w-full'>
-            <p className='text-slate-100 text-lg text-center'>© 2024 Film Freak All Rights Reserved</p>
-        </footer>
-    );
-}
